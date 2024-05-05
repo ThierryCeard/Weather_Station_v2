10fd76c897270179da3bc98d904222b03deab768
Thierry CEARD <thierry.ceard@free.fr>
2024-05-05 16:44:43 -0700

    Working

--- Station_v2.ino
+++ Station_v2.ino
@@ -1,5 +1,3 @@
-// Try to solve include and compilation issues
-
 // Code simplification attempts
 
 // Request JSONified
@@ -11,31 +9,35 @@
 #include <ESP8266HTTPClient.h>
 #include <Wire.h>
 #include <Adafruit_BMP085.h>
-
-
 #include <ArduinoJson.h>
 #include "AS5600.h"
-//#include <LiquidCrystal_I2C.h> // Library for LCD
 #include <DHTesp.h>
+#include <BMP180I2C.h>
+
+#define I2C_ADDRESS 0x77
+
+//create an BMP180 object using the I2C interface
+BMP180I2C bmp(I2C_ADDRESS);
 
 // Cablage sur UART
 // GROUND Marron /  TX BLEU /  RX ORANGE
 
 // Pour Debug decommenter les lignes suivantes
 #define DEBUG_TRACE 
-#define DEBUG_HTTP 
+#define WIFI_CONNECTION 
+//#define DEBUG_HTTP 
 //#define TRACE_DHT 
 #define DEBUG_WIND_DIR 
 //#define DEBUG_WATER
 //#define TIMER
 //#define WIFI_TRACE
-#define BMP180
-#define BMP085_DEBUG 1
+//#define BMP180
+
 
 // Parametres
-#define SAMPLE_RETRY  3e3               // Temps d'attente avant renvoi sur requete invalide
+#define SAMPLE_RETRY  1e3               // Temps d'attente avant renvoi sur requete invalide
 #define COUNT_RETRY   2                 // Nombre de tentative avant de passer a la suite
-#define SLEEPING_TIME 20               // in s
+#define SLEEPING_TIME 40               // in s
 #define WATER_BOUNCING_TIMEMS 500          // Time in ms to debounced 2 water drop
 #define SCREEN_LIGHT_TIME 10           // in s time the display is on
 #define SSID_NETWORK  "ARRIS-CEARD"
@@ -44,21 +46,21 @@
 #define DHT_RETRY     5
 #define URL           "http://thierryceard.synology.me/Meteo/upload_data_JSON.php" // Web Service URL to manage data
 #define HOSTNAME      "OUTER-STATION-456098"
+
 // Les pins de branchement 
 #define DHT_PIN 2                      //GPIO2
-
-#define I2C_SDA 14                    //GPIO 14
-#define I2C_SCL 13                    //GPIO 13
+#define I2C_SDA 14                    //I2C DATA
+#define I2C_SCL 13                    //I2C CLOCK
 #define PIN_WIND_SPEED 0
-//#define SCREEN_INPUT 14              // Pin for screen interrupt
 #define WATER_PIN 5 // 4 For debug should be 5
 
 // Initialization 
-Adafruit_BMP085 bmp;
+
+//Adafruit_BMP085 bmp;
 
 AS5600 as5600;   
 DHTesp dht;
-//LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 20, 4); // Change to (0x27,20,4) for 20x4 LCD.
+
 String        MAC;
 int           Retry=0;
 int           Counter=0;
@@ -67,13 +69,10 @@ int           FallTime=0;
 bool          goodToSend=true;
 bool          isWPSConnected=true;
 int secondElapsed=0; // Time till the last request is sent
-int secondScreen=0; // Time till the screen is on 
-bool screenIsOff=true; // screen State
-bool screenNotDisplayed=false;
 bool isWaterDebounced=false;
 long lastWaterInterrupt=0;
 const String compile_date = __DATE__ " " __TIME__;
-const String compile_version = "2.10.1";
+const String compile_version = "2.1-git";
 const String sketch_name= __FILE__;
 
 //JSON Object
@@ -81,8 +80,6 @@ DynamicJsonDocument object(2048);
 
 void setup()
 {
- 
-
 // Interruption setup for Wind and Water detection
 // ------------------------------------------------
 // Wind Detection 
@@ -105,7 +102,7 @@ timer1_enable(TIM_DIV256, TIM_EDGE, TIM_LOOP);
 timer1_write(312500); // Timer on DIV256 = Frequence : 312.5KHz (second drum beat)
 
 // Pour eviter les pb d'initialisation suite a l'appel de courant au demarrage
-delay(2000);
+delay(500);
  
 // Ouverture de la connexion serie pour tracking
 #ifdef DEBUG_TRACE 
@@ -129,49 +126,32 @@ dht.setup(DHT_PIN, DHTesp::DHT11); // Connect DHT sensor to GPIO 2
 
 // Initiate connexion on pin SDA  and SCL 
 Wire.begin(I2C_SDA,I2C_SCL);
-delay(1000);
+delay(500);
 
-Serial.println("BMP Connection :");
+//Serial.println("BMP Connection :");
 
-if (bmp.begin()) {
+if (!bmp.begin()) {
   #ifdef BMP180
-     Serial.println("BMP180 / BMP085 Connected ");
+    Serial.println("begin() failed. check your BMP180 Interface and I2C Address.");
+		while (1);  
   #endif
 }
 
 // Delai au boot pour debug et reprise de main
 #ifdef DEBUG_TRACE
-Serial.println("Debut delai attente 3s");
-delay(3000);
+Serial.println("Debut delai attente 2s");
+delay(2000);
 #endif
 
   as5600.begin(4);  //  set direction pin.
+  delay(2000);
   as5600.setDirection(AS5600_CLOCK_WISE);  // default, just be explicit.
-
-// Setup du LCD
-/*lcd.init();
-lcd.display();
-lcd.backlight();
-lcd.setCursor(0,1);
-Serial.println("Hello, World sur l'ecran");
-lcd.print("Hello, World!");
-delay(4000);
-
-lcd.noBacklight();
-//lcd.noDisplay();*/
+  int b = as5600.isConnected();
+  Serial.print("Connect AS5600 : ");
+  Serial.println(b);
 }
 
-void loop() {
-
-/*if (!screenIsOff && !screenNotDisplayed) {
-    displayOnScreen();
-    screenNotDisplayed=true;
-}*/
-/*if (screenIsOff) {
-    lcd.noBacklight();
-    lcd.noDisplay();
-}*/
-  
+void loop() {  
 if (goodToSend==true) {
   // object clean up to avoid memory leak
   object.clear();
@@ -189,31 +169,26 @@ if (goodToSend==true) {
   getWindAndWater();
 
   // Send JSON to the server 
+  
   goodToSend=!sendHTTPRequest();
   
-  //  Sleep mode entering :
-  #ifdef DEBUG_TRACE
-  Serial.println("Entering Sleep Mode");
-  #endif
-  
-  WiFi.mode( WIFI_OFF );
-  WiFi.forceSleepBegin();
-  delay(100);
+    //  Sleep mode entering :
+    #ifdef WIFI_CONNECTION
+    Serial.println("Entering Sleep Mode");
+    #endif
+    
+    WiFi.mode( WIFI_OFF );
+    
+    #ifdef WIFI_CONNECTION
+    WiFi.printDiag(Serial);
+    #endif
+    
+    //WiFi.forceSleepBegin();
+    delay(100);
   }
 }
 
-// Pin state : True for High 
- ICACHE_RAM_ATTR void screenInterrupt() {
-     screenIsOff=false;
-     screenNotDisplayed=false;
-     secondScreen=0;
-     #ifdef TIMER
-     Serial.println("Screen interrupt !!! ");Serial.print(int(millis()/1000));
-      #endif
-}
-
- 
-  ICACHE_RAM_ATTR void waterInterrupt() {
+  ICACHE_RAM_ATTR void waterInterrupt() { // Possible issue to fix during millis rollover
   if (((millis()-lastWaterInterrupt)> WATER_BOUNCING_TIMEMS) || (millis()-lastWaterInterrupt)<0) {
     Water++;
     lastWaterInterrupt=millis();  
@@ -231,22 +206,11 @@ if (goodToSend==true) {
     secondElapsed=secondElapsed-SLEEPING_TIME;
     }
 
-  if (!screenIsOff) {
-    if (++secondScreen>SCREEN_LIGHT_TIME) {
-      screenIsOff=true;
-      secondScreen=0;
-      #ifdef TIMER
-      Serial.println("Screen off");
-      #endif
-        }
-  }
 #ifdef TIMER
     Serial.print("Time : "); Serial.println(secondElapsed);
-    Serial.print("Screen Time : "); Serial.println(secondScreen);
 #endif
 }
   
-
 void getDHTMeasure() {
   // DTH Sensor
   // Get Humidty - Temp in Celsius and Heat Index and populate the JSON Object with values 
@@ -281,23 +245,36 @@ void getDHTMeasure() {
   Serial.print("BMP Traces :");
   #endif
   // BMP180  Sensor
-  Serial.println(bmp.readPressure());
-  object["BMP_Pressure"]=bmp.readPressure();
-  object["BMP_Temperature"]=bmp.readTemperature();
+  object["BMP_Temperature"]=bmp.getTemperature();
+  delay(100) ;
+  object["BMP_Pressure"]=bmp.getPressure();
+ 
   delay(100);
   #ifdef BMP180
   Serial.println("BMP Sensor ");
   Serial.print("BMP Pressure :");
-  Serial.print(bmp.readPressure());
+  Serial.print(object["BMP_Pressure"]);
   Serial.print("\t");
   Serial.print("BMP Temperature :");
-  Serial.println(bmp.readTemperature());
+  Serial.println(object["BMP_Temperature"]);
   #endif
+  if (!bmp.measureTemperature())
+	{
+		Serial.println("could not start temperature measurement, is a measurement already running?");
+		return;
+	}
+
+	//wait for the measurement to finish. proceed as soon as hasValue() returned true. 
+	do
+	{
+		delay(100);
+	} while (!bmp.hasValue());
 }
 
 void getWindAndWater() {
   // Wind sensors
   int rawWind=analogRead(PIN_WIND_SPEED);
+  
   int winDir =as5600.rawAngle()* AS5600_RAW_TO_DEGREES;
   #ifdef DEBUG_WIND_DIR  
   Serial.print("Wind Direction  : ");   Serial.print(Counter);
@@ -311,36 +288,18 @@ void getWindAndWater() {
   object["Wind_Dir2"]=winDir;
 }
 
-void displayOnScreen() {
-// Display on screen  
-/*    getDHTMeasure();
-    getBMP180Measure();
-    getWindAndWater();
-    Serial.println("Initialization de l'ecran !");Serial.print(int(millis()/1000));
-    lcd.clear();
-    lcd.display();
-    lcd.backlight();
-    lcd.setCursor(0,0);
-    Serial.println("Debut affichage !!");
-    int windDir= object["Wind_Dir2"];
-    int windSpeed = object["Wind_Speed"];
-    lcd.print("Wind "); lcd.print(windDir);lcd.print("deg");
-    lcd.setCursor(12,0);
-    lcd.print(windSpeed); lcd.print(" km/h");
-    lcd.setCursor(0,1);
-    lcd.print("Temp : ");
-     int win=object["BMP_Temperature"];
-    lcd.print(win);
-    lcd.setCursor(0,2);
-    lcd.print("Pressure : ");
-    win=object["BMP_Pressure"];
-    lcd.print(win);
-    Serial.println("Fin affichage de l'ecran !");*/
-}
+
 
 bool sendHTTPRequest() {
+  // WiFi.printDiag(Serial);
   // Connect to WIFI via PSW or WPS
-  if (!connectWifi()) {
+  bool isWifiConnected = connectWifi();
+  if (!isWifiConnected) {
+    #ifdef WIFI_CONNECTION
+    Serial.print("Wifi Connection Status : ");
+    Serial.print(isWifiConnected);
+    delay(100);
+    #endif
     return false;}
 
   // send the JSON formatted data off all the sensor to the HTTP Web service
@@ -348,19 +307,21 @@ bool sendHTTPRequest() {
   HTTPClient http;  
   WiFiClient wificlient;
   http.begin(wificlient,URL);      
-  //http.addHeader("Content-Type", "application/json"); 
+  http.addHeader("Content-Type", "application/json"); 
   int httpCode=0;  
   String json;
   serializeJson(object,json);
   
   while ((httpCode!=200)&&(Retry<=COUNT_RETRY)) {
-   delay(100);
+   
    httpCode = http.POST(json);   //Send the request
-   String payload = http.getString();  //Get the response payload 
+   
    #ifdef DEBUG_HTTP
+   String payload = http.getString();  //Get the response payload 
    Serial.print("POST : ");  Serial.println(json);
    Serial.print("Http code : "); Serial.println(httpCode);   //Print HTTP return code
    Serial.print("Payload   : "); Serial.println(payload);    //Print request response payload
+   Serial.print("Retry :");Serial.println(Retry);
    #endif
    delay(SAMPLE_RETRY);
    Retry++;
@@ -369,41 +330,53 @@ bool sendHTTPRequest() {
   http.end();  //Close connection
   if (httpCode==200) {
     Water=0; // Water reset to 0 between each request
-    return true;
+    #ifdef DEBUG_HTTP
+    Serial.println("Exiting with true");  
+    #endif
+    return true;  
     }
-  else {return false;}
+  else {
+    #ifdef DEBUG_HTTP
+    Serial.print("Exiting with false"); 
+    #endif
+    
+    return false;}
 }
 
 bool connectWifi() {
+  #ifdef WIFI_CONNECTION
+  Serial.println("Entering connectWifi");
+  delay(100);
+  #endif
+
+  //WiFi.forceSleepWake();
+  //delay(1);
   WiFi.mode(WIFI_STA);
   WiFi.setAutoConnect(true);
   WiFi.setAutoReconnect(true);
   delay(100);
   
+   #ifdef WIFI_CONNECTION
+    WiFi.printDiag(Serial);
+    delay(100);
+    #endif
   int i=0;
   for (i=1;i<=3; i++) {
     switch(i) {
     case 1:
     // Previous connection parameter attempt
-    #ifdef DEBUGTRACE
-    Serial.println(i);
-    #endif
-    WiFi.begin();
+    WiFi.begin(SSID_NETWORK,PASS_WIFI);
     delay(500);
     break;
     // PWD and SSID connection parameter attempt
     case 2:
-    #ifdef DEBUGTRACE
-    Serial.println(i);
-    #endif
     WiFi.begin(SSID_NETWORK,PASS_WIFI);
     delay(500);
     break;
     // WPS Connection
     case 3:
     WiFi.disconnect();
-    #ifdef DEBUGTRACE
-    Serial.print(i);
+    #ifdef WIFI_CONNECTION
     Serial.println(") Please press WPS button on your router.\n Press GPIO5 to continue...");
     #endif
     while(digitalRead(5)) {yield(); }
@@ -422,13 +395,14 @@ bool connectWifi() {
       delay(500);
       j++;
       if (WiFi.status() == WL_CONNECTED) {     
-        #ifdef DEBUGTRACE
-        Serial.print("Connected with :");Serial.println(i);
+        #ifdef WIFI_CONNECTION
+        Serial.print("Connected with option :");Serial.println(i);
         String json;
         serializeJson(object,json);
         Serial.println(json);
         #endif
         return true;
+		
         }  
       }
   }
